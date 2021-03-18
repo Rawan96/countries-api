5f831452fb30df076d0b7e043093ad30502f5aee
rawan <rawan.designer@hotmail.com>
2021-03-18 23:22:58 +0200

    Filter the country by Region

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,8 @@ import { Observable } from 'rxjs';
 import { CountriesService } from 'src/app/services/countries.service';
 import { Country } from '../../interfaces/country-interface';
 
+const REGIONS = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +13,8 @@ import { Country } from '../../interfaces/country-interface';
 export class HomeComponent implements OnInit {
   searchFilter: string | undefined;
   allCountries: Country[] | undefined;
+  regionFilter: string | undefined;
+  regions = REGIONS;
 
   constructor(private countriesService: CountriesService) {}
 
@@ -23,13 +27,19 @@ export class HomeComponent implements OnInit {
   //Search country by name
   get countries() {
     return this.allCountries
-      ? this.allCountries.filter((country: Country) =>
-          this.searchFilter
-            ? country.name
-                .toLowerCase()
-                .includes(this.searchFilter.toLowerCase())
-            : country
-        )
+      ? this.allCountries
+          .filter((country: Country) =>
+            this.searchFilter
+              ? country.name
+                  .toLowerCase()
+                  .includes(this.searchFilter.toLowerCase())
+              : country
+          )
+          .filter((country: Country) =>
+            this.regionFilter
+              ? country.region.includes(this.regionFilter)
+              : country
+          )
       : this.allCountries;
   }
 }
