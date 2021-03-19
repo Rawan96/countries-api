4ce24e57185165f8bb8473134de8bd680aed4b46
rawan <rawan.designer@hotmail.com>
2021-03-19 14:00:32 +0200

    Add the country page functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CountryComponent } from './pages/country/country.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: ':country',
+    component: CountryComponent,
   },
 ];
 
---
+++ src/app/pages/country/country.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CountriesService } from 'src/app/services/countries.service';
+import {
+  Country,
+  Currency,
+  Language,
+} from '../../interfaces/country-interface';
+import { tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-country',
+  templateUrl: './country.component.html',
+  styleUrls: ['./country.component.css'],
+})
+export class CountryComponent implements OnInit {
+  country$: Observable<Country> | undefined;
+
+  constructor(
+    private countries: CountriesService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.country$ = this.countries
+        .getCountryByName(params.country)
+        .pipe(tap((res) => console.log(res)));
+    });
+  }
+
+  getLanguages(languages: Language[]) {
+    return languages.map((language) => language.name);
+  }
+
+  getCurrencies(currencies: Currency[]) {
+    return currencies.map((currency) => currency.name);
+  }
+}
